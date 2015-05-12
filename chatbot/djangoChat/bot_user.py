b46ba3309941a4e22bfdf2b52ac30b0bd87dc2b2
zehavitc <zehavitc@gmail.com>
2015-05-12 17:25:30 +0300

    Add bot messages

--- chatbot/djangoChat/bot_user.py
+++ chatbot/djangoChat/bot_user.py
@@ -1,5 +1,26 @@
-import time
-class bot_user(object):
+from threading import Thread
+from .models import Message
+import wikipedia
 
-    def response(msg):
-        pass
+import time
+
+
+class bot(object):
+    def __init__(self):
+        self.name = "bot user"
+
+    def get_response(self, msg):
+        # return 'Hi, I\'m a human'
+        first_word = msg.split(' ')
+        wikipedia.set_lang('He')
+        value = wikipedia.search(first_word)
+        if len(value) != 0:
+            catagories = wikipedia.WikipediaPage(value[0]).categories
+            if len(catagories) != 0:
+                return catagories[0]
+            return wikipedia.summary(value)
+        return "No result"
+
+
+
+
--- chatbot/djangoChat/views.py
+++ chatbot/djangoChat/views.py
@@ -10,113 +10,115 @@ from .models import Message, ChatUser
 from django.contrib.auth.models import User
 import datetime
 from django.utils.timezone import now as utcnow
+from .bot_user import bot
 
 
 def index(request):
-	if request.method == 'POST':
-		print(request.POST)
-	logged_users = []
-	if request.user.username and request.user.profile.is_chat_user:
-		context = {'logged_users':logged_users}
-		return render(request, 'djangoChat/index.html', context)
-	else:
-		return HttpResponseRedirect(reverse('login'))
+    if request.method == 'POST':
+        print(request.POST)
+    logged_users = []
+    if request.user.username and request.user.profile.is_chat_user:
+        context = {'logged_users': logged_users}
+        return render(request, 'djangoChat/index.html', context)
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
 
 def login(request):
-	if request.user.username and request.user.profile.is_chat_user:
-		return HttpResponseRedirect(reverse('index'))
-	context = {'error':''}	
+    if request.user.username and request.user.profile.is_chat_user:
+        return HttpResponseRedirect(reverse('index'))
+    context = {'error': ''}
 
-	if request.method == 'POST':
-		username = request.POST.get('username','') #retunr '' if no username
-		password = request.POST.get('password','')
+    if request.method == 'POST':
+        username = request.POST.get('username', '')  # retunr '' if no username
+        password = request.POST.get('password', '')
 
-		user = auth.authenticate(username=username,password=password)
+        user = auth.authenticate(username=username, password=password)
 
-		if user is not None:
-			auth.login(request,user)
-			cu = request.user.profile
-			cu.is_chat_user = True
-			cu.last_accessed = utcnow()
-			cu.save()
-			
+        if user is not None:
+            auth.login(request, user)
+            cu = request.user.profile
+            cu.is_chat_user = True
+            cu.last_accessed = utcnow()
+            cu.save()
 
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            context['error'] = ' wrong credentials try again'
+            return render(request, 'djangoChat/login.html', context)
 
-			return HttpResponseRedirect(reverse('index'))
-		else:
-			context['error'] = ' wrong credentials try again'
-			return render(request,'djangoChat/login.html',context)
-
-
-	context.update(csrf(request))		
-	return render(request,'djangoChat/login.html',context)
+    context.update(csrf(request))
+    return render(request, 'djangoChat/login.html', context)
 
 
 def logout(request):
-	cu = request.user.profile
-	cu.is_chat_user = False
-	cu.save()
-	return HttpResponse('succesfully logged out of chat')
+    cu = request.user.profile
+    cu.is_chat_user = False
+    cu.save()
+    return HttpResponse('succesfully logged out of chat')
+
 
 @csrf_exempt
 def chat_api(request):
-	if request.method == 'POST':
-		d = json.loads(request.body)
-		msg =  d.get('msg')
-		user = request.user.username
-		gravatar = request.user.profile.gravatar_url
-		m = Message(user=user,message=msg,gravatar=gravatar)
-		m.save()
+    if request.method == 'POST':
+        d = json.loads(request.body)
+        msg = d.get('msg')
+        user = request.user.username
+        gravatar = request.user.profile.gravatar_url
+        m = Message(user=user, message=msg, gravatar=gravatar)
+        m.save()
+
+        botUser = bot()
+        bot_response = botUser.get_response(m.message)
+        mbot = Message(user=botUser.name, message=bot_response, gravatar=gravatar)
+        mbot.save()
+        res = {'id': m.id, 'msg': m.message, 'user': m.user, 'time': m.time.strftime('%I:%M:%S %p').lstrip('0'),
+               'gravatar': m.gravatar}
+        data = json.dumps(res)
+        # res['msg']='Bot message'
+        # res['user']='bot'
+        # json.dumps(res)
+        return HttpResponse(data, content_type="application/json")
 
 
-		res = {'id':m.id,'msg':m.message,'user':m.user,'time':m.time.strftime('%I:%M:%S %p').lstrip('0'),'gravatar':m.gravatar}
-		data = json.dumps(res)
-		return HttpResponse(data,content_type="application/json")
+    # get request
+    r = Message.objects.order_by('-time')[:70]
+    res = []
+    for msgs in reversed(r):
+        res.append({'id': msgs.id, 'user': msgs.user, 'msg': msgs.message,
+                    'time': msgs.time.strftime('%I:%M:%S %p').lstrip('0'), 'gravatar': msgs.gravatar})
 
+    data = json.dumps(res)
 
-	# get request
-	r = Message.objects.order_by('-time')[:70]
-	res = []
-	for msgs in reversed(r) :
-		res.append({'id':msgs.id,'user':msgs.user,'msg':msgs.message,'time':msgs.time.strftime('%I:%M:%S %p').lstrip('0'),'gravatar':msgs.gravatar})
-	
-	data = json.dumps(res)
-
-	
-	return HttpResponse(data,content_type="application/json")
+    return HttpResponse(data, content_type="application/json")
 
 
 def logged_chat_users(request):
+    u = ChatUser.objects.filter(is_chat_user=True)
 
-	u = ChatUser.objects.filter(is_chat_user=True)
+    for j in u:
+        elapsed = utcnow() - j.last_accessed
+        if elapsed > datetime.timedelta(seconds=35):
+            j.is_chat_user = False
+            j.save()
 
-	for j in u:
-		elapsed = utcnow() - j.last_accessed
-		if elapsed > datetime.timedelta(seconds=35):
+    uu = ChatUser.objects.filter(is_chat_user=True)
 
-			j.is_chat_user = False
-			j.save()
+    d = []
+    for i in uu:
+        d.append({'username': i.username, 'gravatar': i.gravatar_url, 'id': i.userID})
+    data = json.dumps(d)
 
-	uu = ChatUser.objects.filter(is_chat_user=True)
-	
-
-	d = []
-	for i in uu:
-		d.append({'username': i.username,'gravatar':i.gravatar_url,'id':i.userID})
-	data = json.dumps(d)
-	 
-
-	return HttpResponse(data,content_type="application/json")
+    return HttpResponse(data, content_type="application/json")
 
 
 def update_time(request):
-	if request.user.username:
-		u = request.user.profile
+    if request.user.username:
+        u = request.user.profile
 
+        u.last_accessed = utcnow()
+        u.is_chat_user = True
+        u.save()
 
-		u.last_accessed = utcnow()
-		u.is_chat_user = True
-		u.save()
-
-		return HttpResponse('updated')
-	return HttpResponse('who are you?')
+        return HttpResponse('updated')
+    return HttpResponse('who are you?')
